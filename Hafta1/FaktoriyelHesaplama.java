2d0671f571b948fe88cf87160fe049feacba4a7a
CISA <cahitisaoglu10@gmail.com>
2023-09-13 17:58:26 +0300

    Faktoriyel hesaplama

---
+++ Hafta1/FaktoriyelHesaplama.java
@@ -0,0 +1,21 @@
+package Hafta1;
+
+import java.util.Scanner;
+
+public class FaktoriyelHesaplama {
+    public static void main(String[] args) {
+        int n, k, total = 1;
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Lütfen üssü alınacak sayıyı giriniz: ");
+        n = input.nextInt();
+
+        System.out.print("Lütfen üs olacak sayıyı giriniz: ");
+        k = input.nextInt();
+
+        for (int i = 1; i <= k; i++) {
+            total *= n;
+        }
+        System.out.print("Sonuç: " + total);
+    }
+}

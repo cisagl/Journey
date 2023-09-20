0fed96ea205307ab1feb04a1c0e1bea0534a12a0
CISA <cahitisaoglu10@gmail.com>
2023-09-20 19:36:01 +0300

    Update DizidekiElemanlariKucuktenBuyugeSiralama.java

--- Hafta2/DizidekiElemanlariKucuktenBuyugeSiralama.java
+++ Hafta2/DizidekiElemanlariKucuktenBuyugeSiralama.java
@@ -6,17 +6,24 @@ import java.util.Scanner;
 public class DizidekiElemanlariKucuktenBuyugeSiralama {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        // Kullanıcıdan dizinin index sayısını alıyoruz
         System.out.print("Sayı giriniz: ");
-        int a = input.nextInt();
+        int index = input.nextInt();
 
-        int[] liste = new int[a];
+        // Diziyi oluşturuyoruz
+        int[] liste = new int[index];
 
-        for (int i = 0; i < a; i++) {
+        // Kullanıcının kaç sayı gireceğini bilmediğimizden döngü oluşturuyoruz
+        for (int i = 0; i < index; i++) {
+        // Kullanıcıdan tek tek sayıları alıyoruz
             System.out.print((i + 1) + ". sayıyı giriniz: ");
+        // Aldığımız sayıları 0'dan index'e kadar topluyoruz     
             liste[i] = input.nextInt();
         }
 
+        // Bütün sayıları Arrays.sort() ile indexin içerisindeki sayıları küçükten büyüğe sıralıyoruz
         Arrays.sort(liste);
+        // Arrays.toString() ile sıralamayı ekrana çıkarıyoruz
         System.out.println("Girilen sayıların küçükten büyüğe sıralanmış hali: " + Arrays.toString(liste));
     }
 }

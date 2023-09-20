91a6c685c3b1297fb6b1462a729816860b290969
CISA <cahitisaoglu10@gmail.com>
2023-09-20 19:30:20 +0300

    Create DizidekiElemanlariKucuktenBuyugeSiralama.java

---
+++ Hafta2/PatikaPlus/DizidekiElemanlariKucuktenBuyugeSiralama.java
@@ -0,0 +1,22 @@
+package Hafta2.PatikaPlus;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class DizidekiElemanlariKucuktenBuyugeSiralama {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Sayı giriniz: ");
+        int a = input.nextInt();
+
+        int[] liste = new int[a];
+
+        for (int i = 0; i < a; i++) {
+            System.out.print((i + 1) + ". sayıyı giriniz: ");
+            liste[i] = input.nextInt();
+        }
+
+        Arrays.sort(liste);
+        System.out.println("Girilen sayıların küçükten büyüğe sıralanmış hali: " + Arrays.toString(liste));
+    }
+}

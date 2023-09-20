13d4ed917edb4935b1d1c09fe00b8e174ece841a
CISA <cahitisaoglu10@gmail.com>
2023-09-20 14:14:12 +0300

    Gereksiz kodlar silindi

--- Hafta2/MaasHesaplama/Main.java
+++ Hafta2/MaasHesaplama/Main.java
@@ -4,10 +4,9 @@ public class Main {
     public static void main(String[] args) {
 
         Employee employee = new Employee("Patika Dev", 2000, 35, 2020);
-        employee.tax();
-        employee.calculateBonus();
-        employee.raiseSalary();
-        System.out.println(employee.toString());
+        Employee employee2 = new Employee("Fred Jr.", 3000, 55, 2002);
+        employee.toString();
+        employee2.toString();
 
     }
 }

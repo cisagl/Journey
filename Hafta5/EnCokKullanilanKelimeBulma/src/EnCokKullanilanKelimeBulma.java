95d22b0d00cf69bb54251d8ed3aa56c494dd5be9
CISA <cahitisaoglu10@gmail.com>
2023-10-11 12:24:38 +0300

    Update EnCokKullanilanKelimeBulma.java

--- Hafta5/EnCokKullanilanKelimeBulma/src/EnCokKullanilanKelimeBulma.java
+++ Hafta5/EnCokKullanilanKelimeBulma/src/EnCokKullanilanKelimeBulma.java
@@ -8,14 +8,22 @@ public class EnCokKullanilanKelimeBulma {
         System.out.print("Metni giriniz: ");
         String text = input.nextLine();
 
+        // Yeni bir dizi oluşturup kullanıcının girdiği metni dizinin içine atıyoruz
+        // Diziyi boşluklardan ayırıyoruz
         String[] words = text.split(" ");
 
+        // Yeni bir HashMap kullanarak kelime ve kaç kere kullanıldığı bilgisini kayıt ediyoruz
         HashMap<String, Integer> hMap = new HashMap<>();
 
+        //foreach döngüsü ile diziyi geziyoruz
         for (String str : words) {
+            // Eğer HashMap kelimeyi içeriyorsa
             if (hMap.containsKey(str)) {
+                // Kelimenin kaç kere kullanıldığını kayıt etmesi için değişken oluşturuyoruz
                 int times = hMap.get(str);
+                // Key = kelime, Value = kaç kere kullanıldığı
                 hMap.put(str, times + 1);
+                // Eğer HashMap kelimeyi içermiyorsa, kelime + 1
             } else {
                 hMap.put(str, 1);
             }
@@ -24,8 +32,10 @@ public class EnCokKullanilanKelimeBulma {
         String mostUsedWord = "";
         int mostUsedWordNumber = 0;
 
-        for (
-                Map.Entry<String, Integer> entry : hMap.entrySet()) {
+
+        // HashMap'ın kontrolü için yeni bir foreach oluşturuyoruz
+        for (Map.Entry<String, Integer> entry : hMap.entrySet()) {
+            // Eğer gelen yeni değer en çok kullanılan kelimenin sayısından büyükse artık en büyük değer odur
             if (entry.getValue() > mostUsedWordNumber) {
                 mostUsedWord = entry.getKey();
                 mostUsedWordNumber = entry.getValue();
